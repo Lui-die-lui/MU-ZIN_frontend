e558409fc56b5272701d529b38a9a4ff71cccd13
Lui-die-lui <lsg05h@naver.com>
2026-01-14 12:38:44 +0900

    feat: lesson style 변경 로직 작성중

--- src/pages/MyPage/Account/ArtistAccount/LessonStyleSection/LessonStyleSection.tsx
+++ src/pages/MyPage/Account/ArtistAccount/LessonStyleSection/LessonStyleSection.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+/** @jsxImportSource @emotion/react */
+import * as s from "./styles";
+
+const MAX_STYLE_TAGS = 5; // 백엔드에도 동일한 규칙 적용 중
 
 function LessonStyleSection() {
   return (
