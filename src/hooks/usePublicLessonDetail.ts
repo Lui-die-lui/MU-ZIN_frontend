8ddc55e5c1bd85134b9a3e7ce3e5a37feb12e815
Lui-die-lui <lsg05h@naver.com>
2026-04-06 19:31:11 +0900

    fix: data 요청 중복 오류 처리

--- src/hooks/usePublicLessonDetail.ts
+++ src/hooks/usePublicLessonDetail.ts
@@ -4,7 +4,7 @@ import { getPublicLessonDetailReq } from "../apis/lesson/lessonSearchApis";
 export const usePublicLessonDetail = (lessonId: number) =>
   useQuery({
     queryKey: ["lessonDetail", lessonId], // 캐시 자체의 이름표
-    queryFn: async () => (await getPublicLessonDetailReq(lessonId)).data.data, 
+    queryFn: async () => (await getPublicLessonDetailReq(lessonId)).data, 
     // 진짜 데이터를 가져옴 AxiosResponse -> { data: ApiRespDto -> { data: 진짜데이터 } }
     enabled: Number.isInteger(lessonId) && lessonId > 0, // 레슨 아이디가 정수 양수일때만 허용
   })
