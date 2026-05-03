516aab82d2123783b3ca919b05e2592343b7bdb5
Lui-die-lui <lsg05h@naver.com>
2026-05-03 22:42:18 +0900

    feat: main 주석 정리

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,20 +3,7 @@
 import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
 
 function Home() {
-  // const [loading, error] = useKakaoLoader({
-  //   // appkey: import.meta.env.VITE_KAKAO_MAP_JS_KEY,
-  // });
-
-  // if (loading) return <div>지도 불러오는 중...</div>;
-  // if (error) return <div>지도 불러오기 실패</div>;
-
-  return (
-    <Map
-      center={{ lat: 35.1379222, lng: 129.05562775 }}
-      style={{ width: "100%", height: "420px" }}
-      level={4}
-    />
-  );
+  return <>main</>;
 }
 
 export default Home;
